75dc5307bde9ca47cd7128d97af7240f7709c03b
Dan <Stachd@gmail.com>
2020-01-01 21:40:04 -0600

    save point for class conversion

--- neopixel-xmas-tree-2020.py
+++ neopixel-xmas-tree-2020.py
@@ -2,22 +2,58 @@
 ### this code is random suggestion from my family, friends, and other examples on the web. 
 ### orginal code: https://github.com/DanStach/rpi-ws2811
 import time
-#import board
-#import neopixel
-#import random
-#import math
-#import serial
-#import ctypes
-from neopixel_effects import NeopixelEffects
+import board
+import neopixel
+import random
+import math
+import serial
+import ctypes
+from neopixeleffects import NeopixelEffects
 
 
-#param ~microcontroller.Pin pin: The pin to output neopixel data on.
-#param int n: The number of neopixels in the chain
+# param ~microcontroller.Pin pin: The pin to output neopixel data on.
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+pixel_pin = board.D18
+# param int n: The number of neopixels in the chain
+num_pixels = 144
 #param int bpp: Bytes per pixel. 3 for RGB and 4 for RGBW pixels.
+pixel_bpp = 3
 #param float brightness: Brightness of the pixels between 0.0 and 1.0 where 1.0 is full brightness
+pixel_brightness = 0.2
 #param bool auto_write: True if the neopixels should immediately change when set. 
 #                       If False, `show` must be called explicitly.
+pixel_auto_write = False
 #param tuple pixel_order: Set the pixel color channel order. GRBW is set by default.
+ORDER = neopixel.RGB
 
-rocket = NeopixelEffects()
-print("The rocket is at (%d, %d)." % (rocket.x, rocket.y))
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False,
+                           pixel_order=ORDER)
+#pixels = neopixel.NeoPixel(pin=pixel_pin, num_pixels, brightness=pixel_brightness, 
+#                            auto_write=pixel_auto_write, pixel_order=ORDER)
+
+
+effect = NeopixelEffects(pixels)
+
+while True:
+    # Comment this line out if you have RGBW/GRBW NeoPixels
+    pixels.fill((255, 0, 0))
+    # Uncomment this line if you have RGBW/GRBW NeoPixels
+    # pixels.fill((255, 0, 0, 0))
+    pixels.show()
+    time.sleep(1)
+
+    # Comment this line out if you have RGBW/GRBW NeoPixels
+    pixels.fill((0, 255, 0))
+    # Uncomment this line if you have RGBW/GRBW NeoPixels
+    # pixels.fill((0, 255, 0, 0))
+    pixels.show()
+    time.sleep(1)
+
+    # Comment this line out if you have RGBW/GRBW NeoPixels
+    pixels.fill((0, 0, 255))
+    # Uncomment this line if you have RGBW/GRBW NeoPixels
+    # pixels.fill((0, 0, 255, 0))
+    pixels.show()
+    time.sleep(1)
+
+    effect.rainbow_cycle(0.001)    # rainbow cycle with 1ms delay per step
---
+++ neopixeleffects.py
@@ -0,0 +1,42 @@
+import time
+import board
+import neopixel
+
+class NeopixelEffects( ):
+    # Rocket simulates a rocket ship for a game,
+    #  or a physics simulation.
+    
+    def __init__(self, pixels):
+        # Each rocket has an (x,y) position.
+        self.pixels = pixels
+        
+    def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos.i
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos*3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos*3)
+        g = 0
+        b = int(pos*3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos*3)
+        b = int(255 - pos*3)
+    return (r, g, b) if ORDER == neopixel.RGB or ORDER == neopixel.GRB else (r, g, b, 0)
+
+
+    def rainbow_cycle(wait):
+        for j in range(255):
+            for i in range(num_pixels):
+                pixel_index = (i * 256 // num_pixels) + j
+                self.pixels[i] = wheel(pixel_index & 255)
+            self.pixels.show()
+            time.sleep(wait)
